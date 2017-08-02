966bb20d714ecde0bda46e5b1f9daaa88f22a98e
Pawel Kowalczyk <pawel@aurity.co>
2017-08-02 22:23:14 +0200

    test

--- potato/index.android.js
+++ potato/index.android.js
@@ -17,7 +17,7 @@ export default class potato2 extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Finally working!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
