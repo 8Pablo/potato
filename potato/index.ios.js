fe3d7c8afd91aae249198614b64d21a4ec6d66ff
Pawel Kowalczyk <pawel@aurity.co>
2017-08-02 22:18:32 +0200

    text change

--- potato/index.ios.js
+++ potato/index.ios.js
@@ -17,7 +17,7 @@ export default class potato2 extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          It's finally running!
+          It's finally running!!!!!
         </Text>
         <Text style={styles.instructions}>dasd
           To get started, edit index.ios.js
