ef01467b6e3ac86c16f45c4e4eff362b1760e4ba
Gos Sto <gsstlczk@gmail.com>
2017-10-22 19:11:58 +0200

    something till is not working

--- potato/src/candidatesApp/actions/CandidatesActions.js
+++ potato/src/candidatesApp/actions/CandidatesActions.js
@@ -1,12 +1,17 @@
 /**
  * Created by Lena on 18.08.2017.
  */
-import {CANDIDATE_UPDATE, CANDIDATES_FETCH_SUCCESS, CANDIDATE_CREATE, CANDIDATE_SAVE_SUCCESS, CANDIDATE_PREVIEW_NAVIGATE} from './types'
+import {
+  CANDIDATE_UPDATE,
+  CANDIDATES_FETCH_SUCCESS,
+  CANDIDATE_CREATE,
+  CANDIDATE_SAVE_SUCCESS,
+} from './types'
 import firebase from 'firebase';
 
 export const candidateUpdate = ({prop, value}) => {
   return {
-    type   : CANDIDATE_UPDATE,
+    type: CANDIDATE_UPDATE,
     payload: {prop, value}
   };
 };
@@ -74,26 +79,53 @@ export const candidateCreate = ({
 
 };
 
-export const candidateSave = ({name, email, details, status}) => {
+export const candidateSave = ({ name,
+                                email,
+                                level,
+                                jsyears,
+                                feyears,
+                                ghlink,
+                                skills,
+                                project,
+                                notice,
+                                zone,
+                                based,
+                                current,
+                                salary,
+                                whereaurity,
+                                courses,
+                                details,
+                                status,
+                                education
+                              }) => {
   const {currentUser} = firebase.auth();
 
   return (dispatch) => {
     firebase.database().ref(`/users/${currentUser.uid}/candidates/${uid}`)
-            .set({name, email, details, status})
+            .set({
+              name,
+              email,
+              level,
+              jsyears,
+              feyears,
+              ghlink,
+              skills,
+              project,
+              notice,
+              zone,
+              based,
+              current,
+              salary,
+              whereaurity,
+              courses,
+              details,
+              status,
+              education
+            })
             .then(() => {
               dispatch({type: CANDIDATE_SAVE_SUCCESS})
               console.log('saved')
             })
   }
 }
-
-function navigateToNextPage(navigate) {
-  navigate('CandidatesPreview')
-}
-
-export const candidatePreviewNavigate = (navigate) => {
-
-  navigateToNextPage(navigate)
-}
-
 
